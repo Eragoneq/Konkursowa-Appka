4523522ace940a1dd4a4877ece5bc99fa7d5f66f
MetallicGrey <bgolik0810@gmail.com>
2019-10-23 23:18:17 +0200

    +Design itemow w InfoFragment +Odpowiedni layout juz jest +Ikonka ic_cancel

--- app/src/main/java/com/hoffhaxx/app/concurs/activities/InfoPopUpActivity.kt
+++ app/src/main/java/com/hoffhaxx/app/concurs/activities/InfoPopUpActivity.kt
@@ -1,21 +1,12 @@
 package com.hoffhaxx.   app.concurs.activities
 
 import android.os.Bundle
-import android.util.DisplayMetrics
 import androidx.appcompat.app.AppCompatActivity
 import com.hoffhaxx.app.concurs.R
 
 class InfoPopUpActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_infopopup)
-
-        var dm = DisplayMetrics()
-        windowManager.defaultDisplay.getMetrics(dm)
-
-        var width: Int = dm.widthPixels
-        var height: Int = dm.heightPixels
-
-        window.setLayout((width*.8).toInt(), (height*.6).toInt())
+        setContentView(R.layout.info_popup)
     }
 }
