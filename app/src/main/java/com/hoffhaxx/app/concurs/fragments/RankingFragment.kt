6f40b7bd92f56e56b21e122c9dde31c01c1a8e37
michal-molas <molas.mich@gmail.com>
2019-10-23 22:41:54 +0200

    to z zanieczyszczeniem powietrza robie 2

--- app/src/main/java/com/hoffhaxx/app/concurs/fragments/RankingFragment.kt
+++ app/src/main/java/com/hoffhaxx/app/concurs/fragments/RankingFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.TextView
 import com.hoffhaxx.app.concurs.R
 import com.hoffhaxx.app.concurs.misc.SharedPreferencesRepository
@@ -15,25 +16,40 @@ import com.hoffhaxx.app.concurs.misc.SharedPreferencesRepository
  */
 class RankingFragment : Fragment() {
 
+    lateinit var userLocationScore: TextView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
 
     ): View? {
         // Inflate the layout for this fragment
-        val userlocationScore: TextView = view!!.findViewById(R.id.userLocationScore)
-        fun setSmogScore()
-        {
-            
-            //userlocationScore.text =
-        }
 
         return inflater.inflate(R.layout.ranking_fragment, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        userLocationScore = view!!.findViewById(R.id.userLocationScore)
+        setSmogScore()
+    }
+
     private fun createURL() : String {
         val userLocation = SharedPreferencesRepository.userLocation
-        return "https://api.waqi.info/feed/geo:${userLocation!!.latitude};${userLocation!!.longitude}/?token=d62321aeafb151ae88c4a144aab1e9a962175263"
+        if(userLocation != null) {
+            val lat = kotlin.math.round(userLocation.latitude * 100) / 100
+            val lng = kotlin.math.round(userLocation.longitude * 100) / 100
+            return "https://api.waqi.info/feed/geo:$lat;$lng/?token=d62321aeafb151ae88c4a144aab1e9a962175263"
+        }
+        return ""
+    }
+
+    private fun setSmogScore()
+    {
+        var score = 67.toString()
+
+        userLocationScore.text = score
     }
 
 }
