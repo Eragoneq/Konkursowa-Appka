987755b3a971a51bd91443383ef6ccad62c2effb
michal-molas <molas.mich@gmail.com>
2019-10-24 00:09:16 +0200

    działa wyszukiwanie polucji według coordynatów

--- app/src/main/java/com/hoffhaxx/app/concurs/misc/data/PollutionData.kt
+++ app/src/main/java/com/hoffhaxx/app/concurs/misc/data/PollutionData.kt
@@ -2,4 +2,4 @@ package com.hoffhaxx.app.concurs.misc.data
 
 data class Data(val aqi : Int)
 
-data class PollutionData(val status : String, val data : Data)
+data class PollutionData(val data : Data)
