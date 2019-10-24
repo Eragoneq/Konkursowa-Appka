a4e3b1bf41fd2cb04f24b65b73e2c0cbe300376e
Eragoneq <eragon.skill@gmail.com>
2019-10-24 21:49:44 +0200

    Naprawa czytnika zanieczyszczen

--- app/src/main/java/com/hoffhaxx/app/concurs/fragments/RankingFragment.kt
+++ app/src/main/java/com/hoffhaxx/app/concurs/fragments/RankingFragment.kt
@@ -64,24 +64,28 @@ class RankingFragment : Fragment() {
     private fun setPollutionScore() = CoroutineScope(Dispatchers.IO).launch {
         try {
             val userLocation = SharedPreferencesRepository.userLocation
-            if(userLocation != null) {
-                val lat = kotlin.math.round(userLocation.latitude * 100) / 100
-                val lng = kotlin.math.round(userLocation.longitude * 100) / 100
-                val result = PollutionRepository.getAqi(lat, lng)
-                val s = result!!.data.aqi.toString()
-                withContext(Main) {
+            withContext(Main) {
+                if (userLocation != null) {
+                    val lat = kotlin.math.round(userLocation.latitude * 100) / 100
+                    val lng = kotlin.math.round(userLocation.longitude * 100) / 100
+                    val result = PollutionRepository.getAqi(lat, lng)
+                    val s = result!!.data.aqi.toString()
                     userLocationScore.text = s
-                }
-                when {
-                    result.data.aqi<=50 -> userLocationBackground.background = ResourcesCompat.getDrawable(resources, R.color.pollution50Color, null)
-                    result.data.aqi<=100 -> userLocationBackground.background = ResourcesCompat.getDrawable(resources, R.color.pollution100Color, null)
-                    result.data.aqi<=150 -> userLocationBackground.background = ResourcesCompat.getDrawable(resources, R.color.pollution150Color, null)
-                    result.data.aqi<=200 -> userLocationBackground.background = ResourcesCompat.getDrawable(resources, R.color.pollution200Color, null)
-                    result.data.aqi<=300 -> userLocationBackground.background = ResourcesCompat.getDrawable(resources, R.color.pollution300Color, null)
-                    else -> userLocationBackground.background = ResourcesCompat.getDrawable(resources, R.color.pollution300Color, null)
-                }
-            }else{
-                withContext(Main) {
+                    when {
+                        result.data.aqi <= 50 -> userLocationBackground.background =
+                            ResourcesCompat.getDrawable(resources, R.color.pollution50Color, null)
+                        result.data.aqi <= 100 -> userLocationBackground.background =
+                            ResourcesCompat.getDrawable(resources, R.color.pollution100Color, null)
+                        result.data.aqi <= 150 -> userLocationBackground.background =
+                            ResourcesCompat.getDrawable(resources, R.color.pollution150Color, null)
+                        result.data.aqi <= 200 -> userLocationBackground.background =
+                            ResourcesCompat.getDrawable(resources, R.color.pollution200Color, null)
+                        result.data.aqi <= 300 -> userLocationBackground.background =
+                            ResourcesCompat.getDrawable(resources, R.color.pollution300Color, null)
+                        else -> userLocationBackground.background =
+                            ResourcesCompat.getDrawable(resources, R.color.pollution300Color, null)
+                    }
+                } else {
                     userLocationScore.text = "Huj"
                 }
             }
