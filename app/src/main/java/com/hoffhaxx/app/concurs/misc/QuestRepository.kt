f1f64660addf5f0c9d63a79f597e46e304362d4b
Olaf Siwinski <olafsiwinski@gmail.com>
2019-10-25 22:34:54 +0200

    no costam

--- app/src/main/java/com/hoffhaxx/app/concurs/misc/QuestRepository.kt
+++ app/src/main/java/com/hoffhaxx/app/concurs/misc/QuestRepository.kt
@@ -10,13 +10,10 @@ import com.hoffhaxx.app.concurs.web.WebClient
 
 object QuestRepository {
     suspend fun getQuests(amount: Int): MutableList<Quest>? {
-        val quest_list : MutableList<Quest>?    //database
-        val quests : MutableList<Quest>?   //goal
-        var randompos : Int
-
         try {
-            quest_list = WebClient.client.getQuests()?.quests
-            quests = mutableListOf()
+            var randompos : Int
+            val quest_list = WebClient.client.getQuests()?.quests
+            val quests = mutableListOf<Quest>()
 
             for (i in 1..amount){
                 val questssize: Int = quest_list!!.size
@@ -24,10 +21,10 @@ object QuestRepository {
                 quests.add(quest_list[randompos])
             }
             SharedPreferencesRepository.quests = quests
+            return quests
         } catch (e : retrofit2.HttpException) {
             throw WebClient.NetworkException()
         }
-        return quests
     }
 
     suspend fun addQuests(quests : MutableList<Quest>) {
